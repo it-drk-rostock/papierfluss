62bec44e21b506b86b2d015480860584bca8f60a
MarcellusStack <MarcellusStack@gmail.com>
2025-03-27 15:51:01 +0100

    feat: enhance SignInForm with social sign-in functionality and loading state

--- app/_components/sign-in-form.tsx
+++ app/_components/sign-in-form.tsx
@@ -1,8 +1,19 @@
 "use client";
+import { authClient } from "@lib/auth-client";
 import { Paper, Stack, Title, Text, Button } from "@mantine/core";
-import React from "react";
+import React, { useTransition } from "react";
 
 export const SignInForm = () => {
+  const [isPending, startTransition] = useTransition();
+  const signIn = async () => {
+    startTransition(async () => {
+      const data = await authClient.signIn.social({
+        provider: "microsoft",
+        callbackURL: "/dashboard", //the url to redirect to after the sign in
+      });
+    });
+  };
+
   return (
     <Paper withBorder p="md" maw={400} mx="auto">
       <Stack gap="sm">
@@ -11,7 +22,9 @@ export const SignInForm = () => {
           Nach dem Klick auf Anmelden werden Sie weitergeleitet. Geben Sie dort
           Ihre Zugangsdaten für Ihr Firmenkonto ein.
         </Text>
-        <Button>Anmelden</Button>
+        <Button loading={isPending} onClick={signIn}>
+          Anmelden
+        </Button>
       </Stack>
     </Paper>
   );
