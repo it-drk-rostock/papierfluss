6e18896ac4c931be2c0bd050553fa0a5377e06e2
MarcellusStack <Marcellusstack@gmail.com>
2025-04-23 13:17:58 +0200

    feat: add SurveyCreatorWidget component and integrate it into the designer page with necessary styles and dependencies

--- app/(app)/forms/[id]/designer/page.tsx
+++ app/(app)/forms/[id]/designer/page.tsx
@@ -1,9 +1,13 @@
 import { Title } from "@mantine/core";
-
+import { Suspense } from "react";
+import { SurveyCreatorWidget } from "./_components/survey-designer";
 export default function Page() {
   return (
     <>
       <Title order={1}>Designer</Title>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SurveyCreatorWidget />
+      </Suspense>
     </>
   );
 }
