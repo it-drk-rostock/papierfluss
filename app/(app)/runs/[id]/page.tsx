bb025fcf7596f10ea2245e51fc043a0713744f6a
MarcellusStack <MarcellusStack@gmail.com>
2025-06-16 14:19:38 +0200

    feat: add pages and layout for workflow execution with initial content

---
+++ app/(app)/runs/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { Text, Title } from "@mantine/core";
+
+export default function Page() {
+  return (
+    <>
+      <Title order={1}>Workflow Ausführung</Title>
+      <Text>Wählen Sie einen Prozess aus, um die Bearbeitung zu starten.</Text>
+    </>
+  );
+}
