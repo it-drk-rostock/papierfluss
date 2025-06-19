56b689bf17426f476224ab341acb9c2bce614918
MarcellusStack <MarcellusStack@gmail.com>
2025-06-19 10:46:56 +0200

    feat: enhance WorkflowRunTable with action menu and status badge; improve UI for better process management

--- app/(app)/workflows/[id]/_components/workflow-runs-table.tsx
+++ app/(app)/workflows/[id]/_components/workflow-runs-table.tsx
@@ -5,8 +5,13 @@ import "survey-analytics/survey.analytics.tabulator.css";
 import "tabulator-tables/dist/css/tabulator.min.css";
 import { Tabulator } from "survey-analytics/survey.analytics.tabulator";
 import { Model } from "survey-core";
-import { Box, Paper, Stack, Title } from "@mantine/core";
+import { Box, Button, Group, Menu, Paper, Stack, Title } from "@mantine/core";
 import { WorkflowRunsProps } from "../_actions";
+import { WorkflowStatusBadge } from "@/components/workflow-status-badge";
+import { WorkflowStatus } from "@prisma/client";
+import { LinkButton } from "@/components/link-button";
+import { useDisclosure } from "@mantine/hooks";
+import { MenuItemLink } from "@/components/link-menu-item";
 
 interface ProcessRunData {
   id: string;
@@ -28,6 +33,7 @@ interface WorkflowRunTableProps {
 }
 
 export const WorkflowRunTable = ({ workflowRun }: WorkflowRunTableProps) => {
+  const [opened, handlers] = useDisclosure(false);
   let [vizPanel, setVizPanel] = useState<Tabulator>();
 
   useEffect(() => {
@@ -73,9 +79,28 @@ export const WorkflowRunTable = ({ workflowRun }: WorkflowRunTableProps) => {
   return (
     <Paper withBorder p="md" mb="md">
       <Stack>
-        <Title order={4}>
-          Workflow Run: {workflowRun.id} - Status: {workflowRun.status}
-        </Title>
+        <Group>
+          <Title order={4}>Workflow Run: {workflowRun.id}</Title>
+          <WorkflowStatusBadge status={workflowRun.status as WorkflowStatus} />
+          <Menu
+            shadow="md"
+            opened={opened}
+            width={200}
+            closeOnItemClick={false}
+            closeOnClickOutside={false}
+          >
+            <Menu.Target>
+              <Button onClick={handlers.toggle} variant="light">
+                Aktionen
+              </Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <MenuItemLink href={`/runs/${workflowRun.id}`}>
+                Zum Formular
+              </MenuItemLink>
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
         <Box
           style={{ height: "400px", width: "100%" }}
           id={`summaryContainer-${workflowRun.id}`}
