7aaa2cf8a7ce4455b28b1333f2003ca70bd8a3cf
MarcellusStack <marcellusstack@gmail.com>
2025-12-29 14:37:26 +0100

    feat: add Forbidden page and update admin query to use forbidden response

--- server/utils/admin-query.ts
+++ server/utils/admin-query.ts
@@ -1,12 +1,12 @@
 "use server";
 import { getSession } from "./get-session";
-import { redirect } from "next/navigation";
+import { forbidden, redirect } from "next/navigation";
 import { cache } from "react";
 export const adminQuery = cache(async () => {
   const session = await getSession();
 
   if (!session || session.user.role !== "admin") {
-    redirect("/forbidden");
+    forbidden();
   }
 
   return session;
